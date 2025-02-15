4bcaff5a4680414b51d74e36c2983b1e9dfc0e8b
Mark Finean <33692727+mfinean@users.noreply.github.com>
2025-02-15 17:33:34 +0000

    More permissive config

--- rate-my-outfit/next.config.js
+++ rate-my-outfit/next.config.js
@@ -4,55 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'serpapi.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.google.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'fashn.ai',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.cloudfront.net',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.shopify.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.asos-media.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.amazon.co.uk',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn1.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn2.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn3.gstatic.com',
+        hostname: '*',
       }
     ],
   },
